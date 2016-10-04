d1ee76fe32970e38b3841e3af6f15523724ef55d
cassaram09 <cassaram09@gmail.com>
2016-10-04 12:11:03 -0400

    Build application controller
      A number of features are added to the controller:
        - set the user into an instance variable before every controller action
        - configure additional parameters for the Devise User model through :configure_permitted_parameters
        - edit alert message when a user tries to access a page without authorization (for pundit, later)
        - edit omniauth sign in path (for omniauth, later)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # include Pundit
+  protect_from_forgery with: :null_session
+  before_action :set_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role, :name, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform that action."
+    redirect_to(request.referrer || root_path)
+  end
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || root_path
+  end
+  
 end
