7c4af6ca470a3a0ed7104d89a37e3fcf463830f2
cassaram09 <cassaram09@gmail.com>
2016-10-15 11:39:31 -0400

    move enum from messages to gear

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,4 +1,5 @@
 class Gear < ActiveRecord::Base
   belongs_to :user
   belongs_to :lease
+  enum gear_type: [:instrument, :accessory]
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,4 @@
 class Message < ActiveRecord::Base
   belongs_to :sender, class_name: "User"
   belongs_to :recipient, class_name: "User"
-
-  enum :gear_type [:instrument, :accessory]
 end
