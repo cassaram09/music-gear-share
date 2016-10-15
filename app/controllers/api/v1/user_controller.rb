3459aa2fca75f50baee3eb1dceae08a706a16d3f
cassaram09 <cassaram09@gmail.com>
2016-10-15 14:35:01 -0400

    remove unecessary code from usercontroller

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,33 +1,9 @@
 module Api
   module V1
     class UserController < ApplicationController
-      before_action :set_message, except: [:create, :index]
 
       def profile
         render json: @user
-      end
-
-      def update
-        if @message.update(message_params)
-          render json: @message
-        end
-      end
-
-      def destroy
-        @message.destroy
-        render json: message
-      end
-
-      ## PRIVATE METHODS
-
-      private
-
-      def message_params
-        params.require(:message).permit(:content, :sender_id, :recipient_id)
-      end
-
-      def set_message
-        @message = Message.find_by(id: params[:id])
       end
 
     end
